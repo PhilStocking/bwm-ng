04dc417ece5ee0cabc87a5cb3ff36e391842bdd9
Adworks <development@adworks.uk.com>
2018-12-06 12:33:06 +0000

    Add Proxy configuraiton, Add Pipes, Add Details Styles

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,12 +1,16 @@
+import { UppercasePipe } from './../common/pipes/uppercase.pipe';
 import { RentalService } from './shared/rental.service';
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Routes, RouterModule, Router} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+
+import { NgPipesModule } from 'ngx-pipes';
 
 const routes: Routes = [
     {path: 'rentals',
@@ -22,11 +26,14 @@ const routes: Routes = [
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent,
-        RentalDetailComponent
+        RentalDetailComponent,
+        UppercasePipe
     ],
     imports: [
         CommonModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        HttpClientModule,
+        NgPipesModule
     ],
     providers: [RentalService]
 })
